e6f48458cbdb6fba3afd256a5ef25c1322f2e285
Matthew Setter <matthew@matthewsetter.com>
2025-08-04 09:52:44 +1000

    Use PHP Dotenv to load environment variables
    To help keep the app's configuration out of the code, yet load the
    required configuration data as quickly and easily as possible, this
    change introduces PHP Dotenv to load configuration data from a dotenv
    file.

--- public/index.php
+++ public/index.php
@@ -3,12 +3,21 @@
 declare(strict_types=1);
 
 use App\Handler\SmsHandler;
+use Dotenv\Dotenv;
 use Asgrim\MiniMezzio\AppFactory;
 use Mezzio\Router\FastRouteRouter;
 use Mezzio\Router\Middleware\DispatchMiddleware;
 use Mezzio\Router\Middleware\RouteMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+$dotenv->required([
+    'TWILIO_ACCOUNT_SID',
+    'TWILIO_AUTH_TOKEN',
+    'WEATHER_API_KEY',
+])->notEmpty();
 
 $container = require __DIR__ . '/../config/container.php';
 $router = new FastRouteRouter();
